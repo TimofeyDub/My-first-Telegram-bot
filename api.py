acb4c8c463c65cc6b5c0a71104d7ce9733365a12
Dubinin Timofey <162376346+TimofeyDub@users.noreply.github.com>
2026-03-09 19:55:10 +0300

    memchiki

---
+++ api.py
@@ -0,0 +1,20 @@
+import requests
+
+def pogoda():
+    try:
+        #url
+        url = "https://api.open-meteo.com/v1/forecast?latitude=60.015&longitude=30.646&current_weather=true"
+        
+        #данные
+        response = requests.get(url)
+        data = response.json()
+        
+        #температура
+        temperature = data['current_weather']['temperature']
+        
+        #сообщение
+        return f"Погода во Всеше: {temperature}°C"
+        
+    except Exception as e:
+        print(f"Ошибка в api.py: {e}")
+        return "😔 Не удалось получить погоду"
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import telebot
+import random
 from telebot import types
+from api import pogoda
 
 bot = telebot.TeleBot('8379602830:AAFYS3OLfvC5SPw2qEQesZWyE9fAwIZzGvU')
 
-# Обработчик команды /start
+
+
+
+#обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('Жопа')
     btn2 = types.KeyboardButton('Helpaa')
     btn3 = types.KeyboardButton('Мемчик')
+    btn4 = types.KeyboardButton('Погода во Всеше')
     markup.row(btn1, btn2, btn3)
+    markup.row(btn4)
     bot.send_message(message.chat.id, f"Привет @{message.from_user.username}! Это я, Саня Комков. Чем я могу помочь?????", reply_markup=markup)
 
-#Медиа
+#медиа
 @bot.message_handler(content_types=['photo'])
 def getPhoto(message):
     markup = types.InlineKeyboardMarkup()
@@ -23,7 +30,7 @@ def getPhoto(message):
     bot.reply_to(message, 'че за хуйню ты мне отправил?', reply_markup=markup)
 
 
-# Обработчик всех текстовых сообщений
+#обработчик всех текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def on_click(message):
     if message.text == 'Helpaa':
@@ -31,13 +38,17 @@ def on_click(message):
     elif message.text == 'Жопа':
         bot.send_message(message.chat.id, 'Сам(а) ты жопа')
     elif message.text == 'Мемчик':
-            file = open('./photo/1.png', 'rb')
+            memchik = random.randint(1, 3)
+            file = open(f'./photo/{memchik}.png', 'rb')
             bot.send_photo(message.chat.id, file, timeout=60)
             file.close()
     elif message.text.lower() == 'привет':
          bot.send_message(message.chat.id, 'Уже здоровались')
     elif message.text.lower() == 'пока':
          bot.send_message(message.chat.id, 'Вот и иди нахуй')
+    elif message.text == 'Погода во Всеше':
+         a = pogoda()
+         bot.send_message(message.chat.id, a)
 
 #callback
 @bot.callback_query_handler(func=lambda callback: True)
